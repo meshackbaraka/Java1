c98e1d6a4e14a88f1020e59cb30a2629054849e0
meshackbaraka <meshackbarakaopiro@gmail.com>
2021-11-20 12:28:13 +0300

    used javascript variables

---
+++ index.js
@@ -0,0 +1,6 @@
+let firstName = prompt('What is your given name?');
+
+let secondName = prompt('What is your other name?');
+
+let fullName = `Welcome,${firstName} ${secondName} thanks for visiting our website`;
+alert(fullName);
